4767f222e125af1f26a1d094b24ca6e35e93feeb
Anushreeya Gurung <81456513+4nuG@users.noreply.github.com>
2024-01-18 11:24:26 -0500

    Create sphere.py

---
+++ to_solve/sphere.py
@@ -0,0 +1,31 @@
+# Task: Compute various properties of a sphere
+
+# TODO: Your task is to complete the computations below
+# Compute various properties of a sphere based on the provided formulas
+
+# Import the math module to access pi
+import math
+
+# TODO: Get the radius of the sphere from the user
+# Example: r = float(input('Radius? '))
+r = ...
+
+# TODO: Compute the sphere's diameter
+# Example: D = 2 * r
+D = ...
+
+# TODO: Compute the sphere's circumference
+# Example: C = 2 * math.pi * r
+C = ...
+
+# TODO: Compute the sphere's surface area
+# Example: A = 4 * math.pi * r ** 2
+A = ...
+
+# TODO: Compute the sphere's volume
+# Example: V = (4/3) * math.pi * r ** 3
+V = ...
+
+# TODO: Print the computed properties
+# Example: print('Diameter:', D, 'Circumference:', C, 'Area:', A, 'Volume:', V)
+print('Diameter:', D, 'Circumference:', C, 'Area:', A, 'Volume:', V)
